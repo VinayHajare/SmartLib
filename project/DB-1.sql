450f95427ee3a891b32cff717fc88c5724bed50b
Vinay Arjun Hajare <vinayhajare2004@gmail.com>
2023-03-19 14:13:26 +0530

    Added the new table named Borrowers for storing the borrowers data and added some entries for checking or showing functionalities

--- project/DB-1.sql
+++ project/DB-1.sql
@@ -22,19 +22,41 @@ CREATE TABLE Issued_Books
 (
  BK_ID VARCHAR(20) FOREIGN KEY references Books(BK_ID),
  BK_NAME VARCHAR(40),
- BOR_ID VARCHAR(20),
- BOR_NAME VARCHAR(40),
+ BOR_ID VARCHAR(20) FOREIGN KEY references Borrowers(BOR_ID),
+ BOR_NAME VARCHAR(50),
  BOR_CONTACT VARCHAR(40),
  ISS_DATE DATE,
  RET_DATE DATE
 );
+
+--Creating table Borrowers
+CREATE TABLE Borrowers
+(
+ BOR_ID VARCHAR(20),
+ BOR_NAME VARCHAR(50),
+ BOR_ADDRESS VARCHAR(60)
+ BOR_EMAIL VARCHAR(40),
+ BOR_CONTACT VARCHAR(40),
+ BK_BORROWED VARCHAR(20),
+ FINE_AMT VARCHAR(25)
+)
+
 --inserting into books
-insert into books  values(01,'The Alchemist','Paulo Coelho','HarperTorch','500','1');
-insert into books  values(02,'Harry Porter','Harry Porter','Bloomsbury','1000','5');
-insert into books  values(03,'The Brief History of Time','Stephan Hawlking','Bantam Dell','1400','10');
-insert into books  values(04,'Wings Of Fire','A.P.J.Abdul Kalam','Cocoblu','600','15');
-insert into books  values(05,'The World As I See It','Albert Einsten','The Bodley Head','400','25');
+insert into books values(01,'The Alchemist','Paulo Coelho','HarperTorch','500','10');
+insert into books values(02,'Harry Porter','Harry Porter','Bloomsbury','1000','10');
+insert into books values(03,'The Brief History of Time','Stephan Hawlking','Bantam Dell','1400','10');
+insert into books values(04,'Wings Of Fire','A.P.J.Abdul Kalam','Cocoblu','600','10');
+insert into books values(05,'The World As I See It','Albert Einsten','The Bodley Head','400','10');
+insert into books values(06,'Demystifying Computer','Achut Godbole','Tata Mcgraw-Hill','450','10');
+insert into books values(07,'Mein Kamf','Adolf Hitler','Franz Eher Nachfolger','1600','10');
+insert into books values(08,'Indian Polity','M. Lakshmikant','K Sagar','560','10');
+insert into books values(09,'Renaissance State','Girish Kuber','Harper Collins','700','10');
+insert into books values(10,'The Discoverry of India','Jawarlal Nehru','Platkus','800','10');
+
+--inserting into Borrowers
+insert into Borrowers values(01,'Vinay Hajare','Kanersar, Khed, Pune','vinayhajare2004@gmail.com','7559282625','0','0');
+
 --inserting into issued_books
-insert into issued_books values(01,'The Alchemist',01,'Sam Daniel','7559282625',curdate(),curdate()-1);
-insert into issued_books values(02,'Harry Porter',20,'Vivek Hajare','9921837076',curdate(),curdate()+7);
-insert into issued_books values(01,'Wings Of Fire',12,'Sonali Argade','9529282725',curdate(),curdate()+7;
+insert into issued_books values(01,'The Alchemist',01,'Vinay Hajare','7559282625',curdate(),curdate()+4);
+insert into issued_books values(02,'Harry Porter',01,'Vinay Hajare','7559282625',curdate(),curdate()+7);
+insert into issued_books values(04,'Wings Of Fire',01,'Vinay Hajare','7559282625',curdate(),curdate()+8);
