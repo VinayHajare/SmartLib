74aee70d2870599f300381716b6c2a27ece0e971
VinayHajare <vinayhajare2004@gmail.com>
2024-06-18 22:02:23 +0530

    Revert "Revert "Added support to the universal logger for logging various exceptions and events. Added the ExecutorService for asynchronously send email reminders. And some minor bug fixex.""
    This reverts commit b1ecb0106978b96829ff61d06e4cdd4bcbb38a43.

--- SmartLib2.0/SmartLib2.0/src/util/Config.java
+++ SmartLib2.0/SmartLib2.0/src/util/Config.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package core;
+package util;
 
 import java.io.InputStream;
 import java.util.Properties;
@@ -15,14 +15,14 @@ public class Config {
     private static final Properties properties = new Properties();
     
     static{
-        try( InputStream input = Config.class.getClassLoader().getResourceAsStream("config.properties")){
+        try( InputStream input = Config.class.getClassLoader().getResourceAsStream("./resources/config.properties")){
             if( input == null ){
                 System.err.println("Sorry, unable to load/find the config.poperties");
             }else{
                 properties.load(input);
             }
         }catch(Exception e){
-            e.printStackTrace();
+            e.getLocalizedMessage();
         }
     }
     
